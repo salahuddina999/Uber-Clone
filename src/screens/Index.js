541505f0d488223b6cf807b48c7e722ad39f69c8
salahuddina999 <Salahuddina999@gmail.com>
2022-02-18 19:06:07 +0500

    new scrree

---
+++ src/screens/Index.js
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+
+export class Index extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Index</Text>
+      </View>
+    );
+  }
+}
+
+export default Index;
